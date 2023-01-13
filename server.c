77e5927c4cdeed6a2c58a19b6ef26fbd8847633b
Noahcdls <46701760+Noahcdls@users.noreply.github.com>
2023-01-13 08:45:09 -0800

    Initial files
    Initial files

---
+++ server.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/time.h>
