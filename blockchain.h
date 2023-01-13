39c10a3d020abbac29463ab1aad751fb0a4930c8
noahcdls <noahcdls@gmail.com>
2023-01-13 11:40:00 -0800

    starting block chain and found sha256

--- blockchain.h
+++ blockchain.h
@@ -2,14 +2,15 @@
 #include <stdlib.h>
 
 struct{
-    int sender;
-    int recvr;
-    int amount;
+    uint32_t sender;
+    uint32_t recvr;
+    uint32_t amount;
 } block;
 
 struct{
     struct block transaction;
     struct blockchain* prev;
+    uint8_t prev_hash[32];//32*8bit = 256 bit for SHA_256
 }blockchain;
 
 int hash
